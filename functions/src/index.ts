37fd3d3b68f8a965ec3716371b980142e64559ec
Tim Rasim <neogucky@gmail.com>
2019-07-22 22:33:14 +0100

    Added cloud functions to create apple pass

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,76 @@
 import * as functions from 'firebase-functions';
+const { Pass } = require("passkit-generator");
 
+enum Gym {
+    kletterwerk = "Kletterwerk Lübeck",
+    boulderquartier = "Boulderquartier Hamburg",
+    boulderwerk = "Boulderwerk Norderstedt"
+}
 
-export const generate = functions.https.onCall((data, context) => {
-    return "Will generate pass for id: " + data.id;
+export const generateFromStream = functions.https.onRequest((req, res) => {
+
+    let model;
+    switch (req.params.gym) {
+        case 'kletterwerk':
+            model = './generator-hl.pass';
+            break;
+        case 'boulderquartier':
+            model = './generator-hh.pass';
+            break;
+        default:
+            model = './generator.pass';
+    }
+
+    const applePass = new Pass({
+        model: model,
+        certificates: {
+            wwdr: "./certs/wwdr.pem",
+            signerCert: "./certs/signercert.pem",
+            signerKey: {
+                keyFile: "./certs/signerkey.pem",
+                passphrase: "secret123"
+            }
+        },
+        // if true, existing keys added through methods get overwritten
+        // pushed in queue otherwise.
+        shouldOverwrite: true
+    });
+
+    // Adding some settings to be written inside pass.json
+    applePass.barcode([{
+        message: req.query.id,
+        format: "PKBarcodeFormatCode128"
+    }]);
+
+    if (req.query.name !== '') {
+        applePass.secondaryFields.push({
+            "key": "member",
+            "value": req.query.name
+        });
+    }
+
+    if (req.query.gym !== '' && req.query.gym !== 'other') {
+        applePass.auxiliaryFields.push({
+            "key": "location",
+            "value": Gym[req.query.gym]
+        });
+    }
+
+    res.set({
+        "Content-type": "application/vnd.apple.pkpass",
+        "Content-disposition": "attachment; filename=membershippass.pkpass"
+    });
+
+    applePass.generate()
+        .then((stream : any) => {
+            const watchdog = stream.pipe(res);
+
+            watchdog.on('finish', () => {
+                console.log('Finish event emitted');
+                res.end();
+            });
+        })
+        .catch((err : any) => {
+            console.error(err);
+        });
 });
