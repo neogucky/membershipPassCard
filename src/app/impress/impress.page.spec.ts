359db8867a4b334210a21c0370e7d7d2efef01c9
Tim Rasim <neogucky@gmail.com>
2022-02-13 12:03:53 +0100

    Updated Angular to 11

--- src/app/impress/impress.page.spec.ts
+++ src/app/impress/impress.page.spec.ts
@@ -1,5 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ImpressPage } from './impress.page';
 
@@ -7,7 +7,7 @@ describe('ImpressPage', () => {
   let component: ImpressPage;
   let fixture: ComponentFixture<ImpressPage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ImpressPage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
--- src/app/share/share.page.spec.ts
+++ src/app/share/share.page.spec.ts
@@ -1,5 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SharePage } from './share.page';
 
@@ -7,7 +7,7 @@ describe('SharePage', () => {
   let component: SharePage;
   let fixture: ComponentFixture<SharePage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SharePage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
